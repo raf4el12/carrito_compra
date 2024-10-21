f7eaee507de4323db58d8b976f6128bda584d21e
Rafael <gomerorafael121@gmail.com>
2024-10-21 11:20:49 -0500

    Descripción clara de los cambios 2

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import ProductRouter from "./product/router.js";
 import OrderRouter from './order/router.js'
-
+import ClienteRouter from "./cliente/router.js";
 const MainRouter = Router()
 
 MainRouter.use('/product', ProductRouter)
 MainRouter.use('/order', OrderRouter)
-
+MainRouter.use('/client', ClienteRouter);
 export default MainRouter
